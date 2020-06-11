4c9e103d044277743123c0ec205d47ad6de7804a
denis909 <denis909@mail.ru>
2020-06-11 17:15:26 +0300

    n/a

--- config/common.php
+++ config/common.php
@@ -3,23 +3,23 @@
 return [
     'container' => [
         'definitions' => [
-            'backend\theme\MainLayout' => 'denis909\themes\sbadmin2\MainLayout'
-            /*
-            'frontend\theme\AccountMenu' => 'denis909\themes\tabler\AccountMenu',
-            'frontend\theme\Assets' => 'denis909\themes\tabler\Assets',
-            'frontend\theme\Card' => 'denis909\themes\tabler\Card',
-            'frontend\theme\CardMenu' => 'denis909\themes\tabler\CardMenu',
-            'frontend\theme\Cart' => 'denis909\themes\tabler\Cart',
-            'frontend\theme\CartItem' => 'denis909\themes\tabler\CartItem',
-            'frontend\theme\CartMenu' => 'denis909\themes\tabler\CartMenu',
-            'frontend\theme\DetailView' => 'denis909\themes\tabler\DetailView',
-            'frontend\theme\FooterMenu' => 'denis909\themes\tabler\FooterMenu',
-            'frontend\theme\GridView' => 'denis909\themes\tabler\GridView',
-            'frontend\theme\Layout' => 'denis909\themes\tabler\Layout',
-            
-            'frontend\theme\MainMenu' => 'denis909\themes\tabler\MainMenu',
-            'frontend\theme\UserMenu' => 'denis909\themes\tabler\UserMenu'
-            */
+            'backend\theme\ActionMenu' => 'denis909\themes\sbadmin2\ActionMenu',
+            'backend\theme\AlertMessages' => 'denis909\themes\sbadmin2\AlertMessages',
+            'backend\theme\Assets' => 'denis909\themes\sbadmin2\Assets',
+            'backend\theme\Breadcrumbs' => 'denis909\themes\sbadmin2\Breadcrumbs',
+            'backend\theme\DetailView' => 'denis909\themes\sbadmin2\DetailView',
+            'backend\theme\GridView' => 'denis909\themes\sbadmin2\GridView',
+            'backend\theme\Infocard' => 'denis909\themes\sbadmin2\Infocard',
+            'backend\theme\Layout' => 'denis909\themes\sbadmin2\Layout',
+            'backend\theme\LinkPager' => 'denis909\themes\sbadmin2\LinkPager',
+            'backend\theme\LoginLayout' => 'denis909\themes\sbadmin2\LoginLayout',
+            'backend\theme\LoginPage' => 'denis909\themes\sbadmin2\LoginPage',        
+            'backend\theme\MainLayout' => 'denis909\themes\sbadmin2\MainLayout',
+            'backend\theme\MainMenu' => 'denis909\themes\sbadmin2\MainMenu',
+            'backend\theme\Popup' => 'denis909\themes\sbadmin2\Popup',
+            'backend\theme\TopMenu' => 'denis909\themes\sbadmin2\TopMenu',
+            'backend\theme\UserAccount' => 'denis909\themes\sbadmin2\UserAccount',
+            'backend\theme\UserMenu' => 'denis909\themes\sbadmin2\UserMenu'
         ]
     ]
 ];
