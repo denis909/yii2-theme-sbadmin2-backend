7de87bbe205e258b71c666d843ce7edd88e1742f
denis909 <denis909@mail.ru>
2021-02-20 12:00:10 +0300

    n/a

---
+++ common/config/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+use denis909\yii\CascadeConfig;
+
+CascadeConfig::addPath(dirname(dirname(dirname(__DIR__))) . '/yii2-bootstrap4-backend/config');
+CascadeConfig::addPath(dirname(dirname(__DIR__)));
--- config/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use denis909\yii\CascadeConfig;
-
-CascadeConfig::addPath(dirname(dirname(__DIR__)) . '/yii2-bootstrap4-backend/config');
-CascadeConfig::addPath(__DIR__);
